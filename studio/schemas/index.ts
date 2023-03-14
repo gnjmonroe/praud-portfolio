628d9ea725e2d3a9363298b3ab7359fd4b112334
Greg Monroe <gnjmonroe@gmail.com>
2023-03-14 17:36:12 +0900

    Added 'project' schema (#5)

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,6 +1,6 @@
 import blockContent from './blockContent'
-import post from './post'
 
+import project from './project'
 import mediaItem from './mediaItem'
 import aboutPage from './aboutPage'
 import theme from './theme'
@@ -8,13 +8,16 @@ import awardPastNews from './awardPastNews'
 import projectCategory from './projectCategory'
 
 export const stdSlugValidationRegex = /[^\d\-a-zA-Z]+/gm
+export const titleEnValidationRegex = /[^a-zA-Z0-9 ,:_\-.&]+/gm
 
 export const stdSlugValidationErrorMsg =
   'Only latin characters (a-z), numbers (0-9), and hyphens (-) are valid.'
+export const titleEnValidationErrorMsg =
+  'Only alphanumeric characters and certain special characters are allowed ,:_-.'
 
 export const schemaTypes = [
   blockContent,
-  post,
+  project,
   mediaItem,
   aboutPage,
   theme,
---
+++ studio/schemas/project.ts
@@ -0,0 +1,100 @@
+import {defineField, defineType} from 'sanity'
+
+import {
+  stdSlugValidationRegex,
+  stdSlugValidationErrorMsg,
+  titleEnValidationRegex,
+  titleEnValidationErrorMsg,
+} from '.'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'titleEn',
+      title: 'Title (English)',
+      type: 'string',
+      validation: (rule) =>
+        rule.required().custom((currentValue) => {
+          if (currentValue?.match(titleEnValidationRegex)) {
+            return titleEnValidationErrorMsg
+          }
+          return true
+        }),
+    }),
+    defineField({
+      name: 'titleKr',
+      title: 'Title (한국어)',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'titleEn',
+        maxLength: 96,
+      },
+      validation: (rule) =>
+        rule.required().custom((currentValue) => {
+          if (currentValue?.current?.match(stdSlugValidationRegex)) {
+            return stdSlugValidationErrorMsg
+          }
+          return true
+        }),
+    }),
+    defineField({
+      name: 'themes',
+      title: 'Research Themes',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'theme'}}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Project Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'projectCategory'}}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'date',
+      title: 'Project Date',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM',
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'additionalImages',
+      title: 'Additional Images',
+      type: 'array',
+      of: [{type: 'image'}],
+      hidden: ({document}) => !document?.mainImage,
+    }),
+    defineField({
+      name: 'city',
+      title: 'City',
+      type: 'string',
+    }),
+    defineField({
+      name: 'country',
+      title: 'Country',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    }),
+  ],
+})
